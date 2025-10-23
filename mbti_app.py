f4b741d5215c4678259b8b64691a15c23a05c108
Adli Abdillah Nababan <adliabdillahnababan@gmail.com>
2025-10-23 13:10:14 +0700

    Update mbti_app.py

--- mbti_app.py
+++ mbti_app.py
@@ -13,12 +13,14 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_info(dict(st.secrets["gcp_service_account"]), scopes=SCOPE)
+creds = Credentials.from_service_account_info(
+    dict(st.secrets["gcp_service_account"]),
+    scopes=SCOPE
+)
 gc = gspread.authorize(creds)
 
-SHEET_KEY = "1LzT6-aUyW19FygQxycEA820MSPNKXqKHe_7IWBG5FW0"
-worksheet = gc.open_by_key(SHEET_KEY).sheet1
-st.success("✅ Koneksi ke Google Sheet berhasil!")
+# === 3. Koneksi ke Google Sheets via URL ===
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1LzT6-aUyW19FygQxycEA820MSPNKXqKHe_7IWBG5FW0/edit?usp=sharing"
 
 # === 2. Pertanyaan MBTI ===
 questions = {
@@ -157,6 +159,7 @@ if submit:
         st.markdown(f"### 🧠 Hasil MBTI Anda: **{mbti}**")
         st.info(deskripsi)
         st.balloons()
+
 
 
 
